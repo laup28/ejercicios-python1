a40d1c5cd2ee62d545faa4e27aaf8daa272ac632
laup28 <aleja.urrea1997@hotmail.com>
2025-06-28 15:50:36 -0500

    nuevo

---
+++ clase-28/clase1.py
@@ -0,0 +1,16 @@
+"""
+en esta parte vamos a hablar de los conjuntos .set()
+"""
+
+print("CONJUNTOS")
+conjunto = {12, 3, 4, 34, 2, 2, 4, 3, 99, 32}
+print(conjunto)
+
+A = {1, 3, 5, 7, 9}
+B = {2, 4, 6, 8, 10}
+
+print(f'B = {B}')
+print('A.union {B}')
+print()
+print(f'conjunto = {conjunto}')
+conjunto = list
---
+++ clase-28/clase2.py
@@ -0,0 +1,10 @@
+"""
+En este archivo vamos a trabajar con los diccionarios
+"""
+
+clientes = {'Nombre' : ['Juan', 'Pepe', 'Alberto', 'Jose'],
+            'Tel' : [123, 456, 789, 987]},
+
+print(clientes)
+print()
+print('clientes.keys()')
