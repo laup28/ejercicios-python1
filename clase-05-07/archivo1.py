2f0feeacdc2d5372d1dd1577eb5b499ed7516ef8
laup28 <aleja.urrea1997@hotmail.com>
2025-07-05 14:08:07 -0500

    clase 5 julio

---
+++ clase-05-07/archivo1.py
@@ -0,0 +1,16 @@
+"""
+En este archivo vamos a trabajar con programacion con clases (ESTO ESTA EN LE ARCHIVO 26 Q ENVIA EL PROFE)
+"""
+
+class Persona():
+    #Atributos
+    nombre : str
+    documento : int
+    #Funciones
+    def hablar(self):
+        print('Holaaaa')
+        
+        
+class Estidiante(Persona):
+    def __init__(self):
+        pass
